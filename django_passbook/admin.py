365f948d1e1d8d166238dbf660e25230b45945e8
Arkadiy Korotaev <koriaf@yandex.ru>
2018-07-11 17:21:06 +0200

    Minor wording and other improvements
    * more readable http errors
    * ability to use custom pass and pass body getter
    * more readable admin panel
    * bugfix with 'mimetype' usage instead of 'content-type' (old django stuff)

--- django_passbook/admin.py
+++ django_passbook/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django_passbook.models import Pass, Registration, Log
 from django_passbook import settings
-from apns import APNs, Payload
+from apns3 import APNs, Payload
 
 
 def push_update(modeladmin, request, queryset):
@@ -12,12 +12,24 @@ def push_update(modeladmin, request, queryset):
                     key_file=settings.PASSBOOK_CERT_KEY)
         apns.gateway_server.send_notification(r.push_token, Payload())
 
+
 push_update.short_description = "Send a push notification to update Pass"
 
 
+@admin.register(Registration)
 class RegistrationAdmin(admin.ModelAdmin):
+    list_display = ('device_library_identifier', 'push_token', 'pazz')
     actions = [push_update]
 
-admin.site.register(Pass)
-admin.site.register(Registration, RegistrationAdmin)
-admin.site.register(Log)
+
+@admin.register(Pass)
+class PassAdmin(admin.ModelAdmin):
+    list_display = (
+        'serial_number', 'pass_type_identifier', 'authentication_token',
+        'updated_at',
+    )
+
+
+@admin.register(Log)
+class LogAdmin(admin.ModelAdmin):
+    list_display = ('message',)
